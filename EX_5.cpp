17af25e059fd6cfbe972315fe57cf5905d459461
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-12-01 16:54:41 -0300

    EX_5

--- EX_5.cpp
+++ EX_5.cpp
@@ -1,6 +1,6 @@
-/*Ler N nomes e guardar em um vetor, depois imprimi-los na ordem q=ue foram inseridos*/
+/*Ler N nomes e guardar em um vetor, depois imprimi-los na ordem que foram inseridos*/
 
 #include <iostream>
+#include <string>
 #include <vector>
 
-string nomes [3];
