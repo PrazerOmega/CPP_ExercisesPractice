6e36eb69fbcc2e99a08b95e27b981fab07c9a0b0
Kaue Vinicius da Silva <124539389+PrazerOmega@users.noreply.github.com>
2025-12-03 17:01:52 -0300

    EX5 COMPLETE

--- EX_5.cpp
+++ EX_5.cpp
@@ -4,3 +4,27 @@
 #include <string>
 #include <vector>
 
+int main(void){
+
+    int n = 0;
+
+   std:: cout << "Digite a quantia de nomes de deseja escrever: " << std::endl;
+   std:: cin >> n;
+   std::cin.ignore();
+
+   std::string name[n];
+
+   for (size_t i = 0; i < n; i++)
+   {
+    std::cout << "Escreva o nome " << i+1 << std::endl;
+    getline(std::cin, name[i]);
+   }
+    
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << "Nome " << i << ":" << name[i] << std::endl; 
+    }
+    
+
+    return 0;
+}
